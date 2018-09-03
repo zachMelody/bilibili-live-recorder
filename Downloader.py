ddcc31f1525febb641bf1d3eec75989ca92581c4
zachMelody <zhoulv2012@hotmail.com>
2018-09-03 18:54:40 +0800

    update

--- Downloader.py
+++ Downloader.py
@@ -6,9 +6,12 @@ id = '251568'
 bili = BiliBiliLive(id)
 urls = bili.get_live_urls()
 room_info = bili.get_room_info()
-resp = requests.get(urls[0], stream=True)
+if room_info['status']:
+    resp = requests.get(urls[0], stream=True)
+else:
+    raise ZeroDivisionError('该房间还未开始直播')
 
 filename = os.path.join(os.getcwd(), 'files', 'opt.flv')
-with open("files/opt.flv", "wb") as f:
+with open(filename, "wb") as f:
     for chunk in resp.iter_content(chunk_size=512): # 500KB # file_name = 'files/opt_test_{}.flv'.format(str(flag))
         f.write(chunk) if chunk else None
